63ebadda3f5e73d33605acd62f42c30a75c9e7de
shivanij-21 <sj7067699@gmail.com>
2023-11-10 19:17:10 +0530

    changes

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000, // Adjust the delay (in milliseconds) between slides as needed
+    disableOnInteraction: false, // Set to false to continue autoplay even when user interacts with the swiper
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
